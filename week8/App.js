d3bdfb902796cc6d085b93a42ccb1c5b5bc3f121
Rashi-1111 <rashikaur2912@gmail.com>
2025-10-09 19:04:10 +0530

    Add files via upload

---
+++ week8/App.js
@@ -0,0 +1,8 @@
+export default function App(){
+  return `
+    <div style="padding:3rem;text-align:center">
+      <h1>Week 8 Starter</h1>
+      <p>This is a placeholder App.js. Use a bundler (CRA/Vite) to render React components.</p>
+    </div>
+  `;
+}
